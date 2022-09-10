f99085629f1a56cae6de91b11cb8273cb5420a4b
Alen Ricardo <alenricardo08@gmail.com>
2022-09-10 16:37:51 +0000

    Añadimos Alertas y ventanas

---
+++ 05-Alertas y Ventanas/11-Alertas-ventanas.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Alerta, mensaje de alerta
+alert("Este es un mensaje de alerta");
+
+
+// -------------------------------------
+// Alerta de confirmación, es de si o no
+var resultado = confirm("Estas seguro de continuar?");
+// Devuelve un valor verdadero o falso
+console.log(resultado);
+
+// -------------------------------------
+// Alerta de ingreso de datos
+var res = prompt("¿Que edad tienes?", 18);
+// Devuelve datos que escribe el usuario
+console-log(res);
